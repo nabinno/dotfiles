2edabe0fce3e4b2d2a2b3da2c8fbef7d641ac86b
nabinno <nab@blahfe.com>
2014-08-08 23:32:12 +0900

    Modify .zshrc & Add purcell/emacs.d

---
+++ .emacs.d/lisp/init-haskell.el
@@ -0,0 +1,67 @@
+(require-package 'haskell-mode)
+
+;;; Flycheck specifics
+(when (> emacs-major-version 23)
+  (require-package 'flycheck-hdevtools)
+  (require-package 'flycheck-haskell)
+  (after-load 'flycheck
+    (add-hook 'haskell-mode-hook #'flycheck-haskell-setup)
+
+    (defun sanityinc/flycheck-haskell-reconfigure ()
+      "Reconfigure flycheck haskell settings, e.g. after changing cabal file."
+      (interactive)
+      (unless (eq major-mode 'haskell-mode)
+        (error "Expected to be in haskell-mode"))
+      (flycheck-haskell-clear-config-cache)
+      (flycheck-haskell-configure)
+      (flycheck-mode -1)
+      (flycheck-mode))
+
+    (defadvice haskell-mode-stylish-buffer (around skip-if-flycheck-errors activate)
+      "Don't run stylish-buffer if the buffer appears to have a syntax error.
+This isn't a hard guarantee, since flycheck might sometimes not run until the file has
+been saved."
+      (unless (flycheck-has-current-errors-p 'error)
+        ad-do-it))
+
+    (require 'flycheck-hdevtools)))
+
+
+(dolist (hook '(haskell-mode-hook inferior-haskell-mode-hook haskell-interactive-mode-hook))
+  (add-hook hook 'turn-on-haskell-doc-mode)
+  (add-hook hook (lambda () (subword-mode +1))))
+(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+
+(after-load 'haskell-interactive-mode
+  (diminish 'interactive-haskell-mode " IntHS"))
+
+(add-auto-mode 'haskell-mode "\\.ghci\\'")
+
+(require-package 'hi2)
+(add-hook 'haskell-mode-hook 'turn-on-hi2)
+
+(add-hook 'haskell-mode-hook 'haskell-auto-insert-module-template)
+
+(setq-default haskell-stylish-on-save t)
+
+(after-load 'haskell-mode
+  (define-key haskell-mode-map (kbd "C-c h") 'hoogle)
+  (define-key haskell-mode-map (kbd "C-o") 'open-line))
+
+(when (eval-when-compile (>= emacs-major-version 24))
+  (require-package 'ghci-completion)
+  (add-hook 'inferior-haskell-mode-hook 'turn-on-ghci-completion))
+
+(eval-after-load 'page-break-lines
+  '(push 'haskell-mode page-break-lines-modes))
+
+;; Make compilation-mode understand "at blah.hs:11:34-50" lines output by GHC
+(after-load 'compile
+  (let ((alias 'ghc-at-regexp))
+    (add-to-list
+     'compilation-error-regexp-alist-alist
+     (list alias " at \\(.*\\.\\(?:l?[gh]hs\\|hi\\)\\):\\([0-9]+\\):\\([0-9]+\\)-[0-9]+$" 1 2 3 0 1))
+    (add-to-list
+     'compilation-error-regexp-alist alias)))
+
+(provide 'init-haskell)
---
+++ .emacs.d/lisp/init-osx-keys.el
@@ -0,0 +1,17 @@
+(when *is-a-mac*
+  (setq mac-command-modifier 'meta)
+  (setq mac-option-modifier 'none)
+  (setq default-input-method "MacOSX")
+  ;; Make mouse wheel / trackpad scrolling less jerky
+  (setq mouse-wheel-scroll-amount '(1
+                                    ((shift) . 5)
+                                    ((control))))
+  (global-set-key (kbd "M-`") 'ns-next-frame)
+  (global-set-key (kbd "M-h") 'ns-do-hide-emacs)
+  (after-load 'nxml-mode
+    (define-key nxml-mode-map (kbd "M-h") nil))
+  (global-set-key (kbd "M-ˍ") 'ns-do-hide-others) ;; what describe-key reports for cmd-option-h
+  )
+
+
+(provide 'init-osx-keys)
