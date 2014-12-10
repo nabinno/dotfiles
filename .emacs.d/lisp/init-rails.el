a465d372ed65c1cf0eb083d3ba3676d3e3b27588
nabinno <nab@blahfe.com>
2014-12-10 19:19:08 +0900

    Modify init-flycheck, init-ido and init-rails

--- .emacs.d/lisp/init-rails.el
+++ .emacs.d/lisp/init-rails.el
@@ -9,25 +9,41 @@
     (shell-command (concat "ctags -a -e -f " rinari-tags-file-name " --tag-relative -R app lib vendor test"))))
 
 
-
 ; temporary sets
 ; ==============
-;; yasnippet
-(require-package 'yasnippet)
-(yas-global-mode 1)
-(yas-load-directory "~/.emacs.d/site-lisp/yasnippets-rails/rails-snippets")
-(yas-load-directory "~/.emacs.d/site-lisp/yasnippets-rspec/rspec-snippets")
-(yas-reload-all)
-(define-key yas-minor-mode-map (kbd "M-B") 'yas-insert-snippet)
-(require-package 'dropdown-list)
-(setq yas-prompt-functions '(yas-dropdown-prompt
-                             yas-ido-prompt
-                             yas-completing-prompt))
+;; ;; yasnippet
+;; (require-package 'yasnippet)
+;; (yas-global-mode 1)
+;; (yas-load-directory "~/.emacs.d/site-lisp/yasnippets-rails/rails-snippets")
+;; (yas-load-directory "~/.emacs.d/site-lisp/yasnippets-rspec/rspec-snippets")
+;; (yas-reload-all)
+;; (define-key yas-minor-mode-map (kbd "M-B") 'yas-insert-snippet)
+;; (require-package 'dropdown-list)
+;; (setq yas-prompt-functions '(yas-dropdown-prompt
+;;                              yas-ido-prompt
+;;                              yas-completing-prompt))
+
 ;; rspec-mode
-(require-package 'rspec-mode)
 (eval-after-load 'rspec-mode
- '(rspec-install-snippets))
-(rspec-mode 1)
+  '(rspec-install-snippets))
+(require-package 'rspec-mode)
+
+;; rinari-rgrep
+(setq rinari-rgrep-file-endings "*.rb *.erb *.yml *.coffee *.js *.scss *.rake")
+(eval-after-load "grep"
+  '(progn
+     (add-to-list 'grep-find-ignored-directories ".old")
+     (add-to-list 'grep-find-ignored-directories ".sass-cache")
+     (add-to-list 'grep-find-ignored-directories "bin")
+     (add-to-list 'grep-find-ignored-directories "bower_components")
+     (add-to-list 'grep-find-ignored-directories "commons")
+     (add-to-list 'grep-find-ignored-directories "commons.min")
+     (add-to-list 'grep-find-ignored-directories "db")
+     (add-to-list 'grep-find-ignored-directories "fonts")
+     (add-to-list 'grep-find-ignored-directories "log")
+     (add-to-list 'grep-find-ignored-directories "node_modules")
+     (add-to-list 'grep-find-ignored-directories "tmp")
+     (add-to-list 'grep-find-ignored-directories "vendor")))
 
 
 
