af8d46bc185ffe788ab57c786902fa0c2528dc65
Nab Inno <nab@blahfe.com>
2014-12-10 19:19:08 +0900

    Modify init-flycheck, init-ido and init-rails

--- .emacs.d/lisp/init-ido.el
+++ .emacs.d/lisp/init-ido.el
@@ -7,9 +7,9 @@
 (setq ido-auto-merge-work-directories-length 0)
 (setq ido-use-virtual-buffers t)
 
-(when (eval-when-compile (>= emacs-major-version 24))
- (require-package 'ido-ubiquitous)
- (ido-ubiquitous-mode t))
+;; (when (eval-when-compile (>= emacs-major-version 24))
+;;  (require-package 'ido-ubiquitous)
+;;  (ido-ubiquitous-mode t))
 
 ;; Use smex to handle M-x
 (when (eval-when-compile (>= emacs-major-version 24))
