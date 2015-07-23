892411c98e1fd1cbb92b49e79136d7d7b2f76195
nabinno <nab@blahfe.com>
2015-07-23 19:32:44 +0900

    Modify init setting for JavaScript and Git

--- .emacs.d/lisp/init-git.el
+++ .emacs.d/lisp/init-git.el
@@ -1,7 +1,7 @@
 (require-package 'magit)
 (require-package 'git-blame)
-(require-package 'git-commit-mode)
-(require-package 'git-rebase-mode)
+;; (require-package 'git-commit-mode)
+;; (require-package 'git-rebase-mode)
 (require-package 'gitignore-mode)
 (require-package 'gitconfig-mode)
 (require-package 'git-messenger) ;; Though see also vc-annotate's "n" & "p" bindings
@@ -15,7 +15,7 @@
 
 ;; Hint: customize `magit-repo-dirs' so that you can use C-u M-F12 to
 ;; quickly open magit on any one of your projects.
-(global-set-key [(meta f12)] 'magit-status)
+(global-set-key (kbd "ESC <f12>") 'magit-status)
 
 (after-load 'magit
   (define-key magit-status-mode-map (kbd "C-M-<up>") 'magit-goto-parent-section))
@@ -25,14 +25,14 @@
   (fullframe magit-status magit-mode-quit-window))
 
 
-;;; When we start working on git-backed files, use git-wip if available
-
-(after-load 'magit
-  (global-magit-wip-save-mode)
-  (diminish 'magit-wip-save-mode))
-
-(after-load 'magit
-  (diminish 'magit-auto-revert-mode))
+;; ;;; When we start working on git-backed files, use git-wip if available
+;;
+;; (after-load 'magit
+;;   (global-magit-wip-save-mode)
+;;   (diminish 'magit-wip-save-mode))
+;;
+;; (after-load 'magit
+;;   (diminish 'magit-auto-revert-mode))
 
 
 (when *is-a-mac*
@@ -46,35 +46,35 @@
 
 
 
-;;; git-svn support
-
-(require-package 'magit-svn)
-(autoload 'magit-svn-enabled "magit-svn")
-(defun sanityinc/maybe-enable-magit-svn-mode ()
-  (when (magit-svn-enabled)
-    (magit-svn-mode)))
-(add-hook 'magit-status-mode-hook #'sanityinc/maybe-enable-magit-svn-mode)
-
-(after-load 'compile
-  (dolist (defn (list '(git-svn-updated "^\t[A-Z]\t\\(.*\\)$" 1 nil nil 0 1)
-                      '(git-svn-needs-update "^\\(.*\\): needs update$" 1 nil nil 2 1)))
-    (add-to-list 'compilation-error-regexp-alist-alist defn)
-    (add-to-list 'compilation-error-regexp-alist (car defn))))
-
-(defvar git-svn--available-commands nil "Cached list of git svn subcommands")
-
-(defun git-svn (dir)
-  "Run a git svn subcommand in DIR."
-  (interactive "DSelect directory: ")
-  (unless git-svn--available-commands
-    (setq git-svn--available-commands
-          (sanityinc/string-all-matches
-           "^  \\([a-z\\-]+\\) +"
-           (shell-command-to-string "git svn help") 1)))
-  (let* ((default-directory (vc-git-root dir))
-         (compilation-buffer-name-function (lambda (major-mode-name) "*git-svn*")))
-    (compile (concat "git svn "
-                     (ido-completing-read "git-svn command: " git-svn--available-commands nil t)))))
+;; ;;; git-svn support
+;;
+;; (require-package 'magit-svn)
+;; (autoload 'magit-svn-enabled "magit-svn")
+;; (defun sanityinc/maybe-enable-magit-svn-mode ()
+;;   (when (magit-svn-enabled)
+;;     (magit-svn-mode)))
+;; (add-hook 'magit-status-mode-hook #'sanityinc/maybe-enable-magit-svn-mode)
+;;
+;; (after-load 'compile
+;;   (dolist (defn (list '(git-svn-updated "^\t[A-Z]\t\\(.*\\)$" 1 nil nil 0 1)
+;;                       '(git-svn-needs-update "^\\(.*\\): needs update$" 1 nil nil 2 1)))
+;;     (add-to-list 'compilation-error-regexp-alist-alist defn)
+;;     (add-to-list 'compilation-error-regexp-alist (car defn))))
+;;
+;; (defvar git-svn--available-commands nil "Cached list of git svn subcommands")
+;;
+;; (defun git-svn (dir)
+;;   "Run a git svn subcommand in DIR."
+;;   (interactive "DSelect directory: ")
+;;   (unless git-svn--available-commands
+;;     (setq git-svn--available-commands
+;;           (sanityinc/string-all-matches
+;;            "^  \\([a-z\\-]+\\) +"
+;;            (shell-command-to-string "git svn help") 1)))
+;;   (let* ((default-directory (vc-git-root dir))
+;;          (compilation-buffer-name-function (lambda (major-mode-name) "*git-svn*")))
+;;     (compile (concat "git svn "
+;;                      (ido-completing-read "git-svn command: " git-svn--available-commands nil t)))))
 
 
 (require-package 'git-messenger)
