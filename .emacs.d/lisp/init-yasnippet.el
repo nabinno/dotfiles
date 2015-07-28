a49cd9fe896d81a69be03e507a96b50d2123ca13
Nab Inno <nab@blahfe.com>
2015-07-28 21:08:31 +0900

    Modify yasnippet

---
+++ .emacs.d/lisp/init-yasnippet.el
@@ -0,0 +1,16 @@
+(require-package 'yasnippet)
+
+(yas-global-mode 1)
+;; (yas-load-directory "~/.emacs.d/site-lisp/yasnippets-rails/rails-snippets")
+;; (yas-load-directory "~/.emacs.d/site-lisp/yasnippets-rspec/rspec-snippets")
+(yas-reload-all)
+;; (define-key yas-minor-mode-map (kbd "M-B") 'yas-insert-snippet)
+
+(require-package 'dropdown-list)
+;; (setq yas-prompt-functions '(yas-dropdown-prompt
+;;                              yas-ido-prompt
+;;                              yas-completing-prompt))
+
+
+
+(provide 'init-yasnippet)
