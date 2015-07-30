bd7341eef501c4ea11e09a65b663236e48f2f101
Nab Inno <nab@blahfe.com>
2015-07-30 19:21:33 +0900

    Add gulpjs-mode settings

--- .emacs.d/lisp/init-markdown.el
+++ .emacs.d/lisp/init-markdown.el
@@ -3,4 +3,10 @@
 (setq auto-mode-alist
       (cons '("\\.\\(md\\|markdown\\)\\'" . markdown-mode) auto-mode-alist))
 
+(add-hook 'markdown-mode-hook
+          (lambda ()
+            (define-key markdown-mode-map (kbd "C-i") 'markdown-cycle)
+                (hide-sublevels 2)))
+
+
 (provide 'init-markdown)
