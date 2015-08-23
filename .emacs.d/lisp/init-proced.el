b13cacba10a5e1f57deab147f0a7f5ea6ff94390
nabinno <nab@blahfe.com>
2015-08-23 12:59:57 +0000

    Add keybinds.

--- .emacs.d/lisp/init-proced.el
+++ .emacs.d/lisp/init-proced.el
@@ -1,21 +1,31 @@
 ;;; init-proced -- proced configuration
 ;;; Commentary:
 ;;; Code:
-(defun proced-settings ()
-  "Setup proced configuration."
-  (interactive)
-  (progn
-    (proced-toggle-auto-update)
-    (proced-toggle-tree)
-    (proced-sort-interactive "args")
-    (define-key proced-mode-map (kbd "j") 'next-line)
-    (define-key proced-mode-map (kbd "k") 'previous-line)
-    (define-key proced-mode-map (kbd " ") 'proced-mark)
-    (define-key proced-mode-map (kbd "M-a") 'proced-unmark-all)
-    (define-key proced-mode-map (kbd "d") 'proced-send-signal)
-    (define-key proced-mode-map (kbd "D") (lambda () (proced-send-signal "TERM")))
-    ))
-(add-hook 'proced-mode-hook 'proced-settings)
+(add-hook 'proced-mode-hook
+          (lambda ()
+            (progn
+              (proced-toggle-auto-update 1)
+              (proced-toggle-tree 1)
+              (proced-sort-interactive 'args "")
+              (define-key proced-mode-map (kbd "j") 'next-line)
+              (define-key proced-mode-map (kbd "k") 'previous-line)
+              (define-key proced-mode-map (kbd "p")
+                (lambda ()
+                  (interactive)
+                  (progn (beginning-of-buffer) (next-line) (previous-line))))
+              (define-key proced-mode-map (kbd "n")
+                (lambda ()
+                  (interactive)
+                  (progn (end-of-buffer) (previous-line))))
+              (define-key proced-mode-map (kbd "<SPC>") 'proced-mark)
+              (define-key proced-mode-map (kbd "M-a") 'proced-unmark-all)
+              (define-key proced-mode-map (kbd "d") 'proced-send-signal)
+              (define-key proced-mode-map (kbd "D")
+                (lambda ()
+                  (interactive)
+                  (progn (proced-send-signal "TERM")
+                         (revert-buffer))))
+              )))
 
 ;; keybinds
 (global-set-key (kbd "") 'proced)
