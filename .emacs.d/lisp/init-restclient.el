5ad5f96d34ec32b06f6a9ccf9b77135868949339
Nab Inno <nab@blahfe.com>
2015-08-24 11:43:59 +0900

    Add restclient.

---
+++ .emacs.d/lisp/init-restclient.el
@@ -0,0 +1,9 @@
+;;; init-restclient -- restclient configuration
+;;; Commentary:
+;;; Code:
+(require-package 'restclient)
+;; (require-package 'company-restclient)
+
+
+(provide 'init-restclient)
+;;; init-restclient.el ends here
