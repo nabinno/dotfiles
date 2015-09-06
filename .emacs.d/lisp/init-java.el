3049d0d42a3118a74a053886abf5a992918e9f65
Nab Inno <nab@blahfe.com>
2015-09-06 14:43:27 +0000

    Add init files of helm and java.

--- .emacs.d/lisp/init-java.el
+++ .emacs.d/lisp/init-java.el
@@ -1,31 +1,57 @@
 ;;; init-java.el --- basic java configuration
 ;;; Commentary:
 ;;; Code:
-(setenv "JAVA_HOME" "/usr/lib/jvm/java-7-openjdk-amd64/")
 
-
-;;; Malabar-mode
+;;; IDE
+;; malabar-mode
 (require-package 'malabar-mode)
 (add-auto-mode 'malabar-mode "\\.java\\'")
-(when (require 'malabar-mode nil t)
-  (setq malabar-groovy-lib-dir (expand-file-name "~/.emacs.d/malabar/lib"))
-  (add-to-list 'auto-mode-alist '("\\.java\\'" . malabar-mode))
-  ;; Exclude no-using packages from import lists
-  (add-to-list 'malabar-import-excluded-classes-regexp-list "^java\\.awt\\..*$")
-  (add-to-list 'malabar-import-excluded-classes-regexp-list "^com\\.sun\\..*$")
-  (add-to-list 'malabar-import-excluded-classes-regexp-list "^org\\.omg\\..*$")
-  (add-hook 'malabar-mode-hook
-            (lambda ()
-              (add-hook 'after-save-hook 'malabar-compile-file-silently
-                        nil t))))
+;; (when (require 'malabar-mode nil t)
+;;   (setq malabar-groovy-lib-dir (expand-file-name "~/.emacs.d/malabar/lib"))
+;;   (add-to-list 'auto-mode-alist '("\\.java\\'" . malabar-mode))
+;;   ;; Exclude no-using packages from import lists
+;;   (add-to-list 'malabar-import-excluded-classes-regexp-list "^java\\.awt\\..*$")
+;;   (add-to-list 'malabar-import-excluded-classes-regexp-list "^com\\.sun\\..*$")
+;;   (add-to-list 'malabar-import-excluded-classes-regexp-list "^org\\.omg\\..*$")
+;;   (add-hook 'malabar-mode-hook
+;;             (lambda ()
+;;               (add-hook 'after-save-hook 'malabar-compile-file-silently
+;;                         nil t))))
 (after-load 'malabar-mode
   (add-hook 'malabar-mode-hook 'sanityinc/lisp-setup)
   (add-hook 'malabar-mode-hook 'subword-mode))
 (setq malabar-groovy-java-options '("-Duser.language=en"))
 
+;; jdee
+(require-package 'jdee)
+
 
-;;; Jtags
+;;; Maven
+(require-package 'mvn)
+
+;; javadoc
+(require-package 'javadoc-lookup)
+
+;; Add and reorder Java import statements in Maven projects
+(require-package 'javaimp)
+
+;; software testing
+(require-package 'maven-test-mode)
+
+
+;;; Ant
+(require-package 'ant)
+
+
+;;; Other
+;; jtags
 (require-package 'jtags)
+
+;; snipetts
+(require-package 'java-snippets)
+
+;; thread dump
+(require-package 'thread-dump)
 
 
 
