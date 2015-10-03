fae7301378f3618527fa397e140b968546d8d554
nabinno <nab@blahfe.com>
2015-10-03 17:27:47 +0900

    Modify path of install.sh.

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -u
 
 cd ~/
-git clone https://github.com/nabinno/dotfiles.git
-find ~/dotfiles -maxdepth 1 -mindepth 1 | xargs -i mv -f {} ~/
+mkdir -p .local
+git clone https://github.com/nabinno/dotfiles.git .local/
+find ~/dotfiles -maxdepth 1 -mindepth 1 | xargs -i cp -fR {} ~/
 rm -fr .git Dockerfile README.md
 ``
 printf "${GREEN}"
