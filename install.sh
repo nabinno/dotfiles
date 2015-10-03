e757391500982b248a73b115312bb19741fb324e
nabinno <nab@blahfe.com>
2015-10-03 17:33:13 +0900

    Modify condition of install.sh.

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -u
 
 cd ~/
-mkdir -p .local
+if [ ! -d ~/.local ]; then mkdir -p .local; fi
 git clone https://github.com/nabinno/dotfiles.git .local/
-find ~/dotfiles -maxdepth 1 -mindepth 1 | xargs -i cp -fR {} ~/
+find ~/.local/dotfiles -maxdepth 1 -mindepth 1 | xargs -i cp -fR {} ~/
 rm -fr .git Dockerfile README.md
 ``
 printf "${GREEN}"
