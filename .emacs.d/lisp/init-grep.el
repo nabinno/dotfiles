e40facf5641e12b8b3dca625a0aea8985626c641
Nab Inno <nab@blahfe.com>
2015-10-09 12:17:32 +0900

    Add ag-visit-buffer-other-window-noselect-and-next-error method.

--- .emacs.d/lisp/init-grep.el
+++ .emacs.d/lisp/init-grep.el
@@ -8,7 +8,7 @@
 (setq ag-reuse-buffers 'nil)
 
 (defun ag-visit-buffer-other-window (&optional event noselect)
-  "Visit ag result in another window."
+  "Visit ag result in another window with EVENT and NOSELECT."
   (interactive)
   (let ((current-window (selected-window)))
     ;; ag-mode uses compile-goto-error for visiting the result
@@ -16,15 +16,22 @@
     (when noselect
       (select-window current-window))))
 (defun ag-visit-buffer-other-window-noselect (&optional event)
-  "Visit ag result in another window, but don't select it."
+  "Visit ag result in another window with EVENT, but don't select it."
   (interactive)
   (ag-visit-buffer-other-window event t))
+(defun ag-visit-buffer-other-window-noselect-and-next-error ()
+  "Visit ag result in another window but don't select it, and go to next file."
+  (interactive)
+  (progn
+    (ag-visit-buffer-other-window-noselect)
+    (compilation-next-error)))
 
 ;; keybind
 (add-hook 'ag-mode-hook
           (lambda ()
             ;; make C-o and o behave as in dired
             (define-key ag-mode-map (kbd "h") 'ag-visit-buffer-other-window-noselect)
+            (define-key ag-mode-map (kbd "m") 'ag-visit-buffer-other-window-noselect-and-next-error)
             (define-key ag-mode-map (kbd "o") 'ag-visit-buffer-other-window)
             (define-key ag-mode-map (kbd "J") 'next-error-no-select)
             (define-key ag-mode-map (kbd "K") 'previous-error-no-select)
