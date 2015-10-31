574e8be9a805ed07e32e1bc2549d8648a442d89e
nabinno <nab@blahfe.com>
2015-10-31 20:13:00 +0900

    Add init-sekka configuration.

---
+++ .emacs.d/lisp/init-sekka.el
@@ -0,0 +1,11 @@
+;;; init-sekka --- sekka configuration
+;;; Commentary:
+;;; Code:
+(require-package 'sekka)
+(require 'sekka)
+(global-sekka-mode 1)
+
+
+
+(provide 'init-sekka)
+;;; init-sekkaael ends here.
