24594448e3550c340c4333aef122028c3c9fc172
Nab Inno <nab@blahfe.com>
2015-11-03 16:04:02 +0900

    Add my-mu4e-set-account function.

--- .emacs.d/lisp/init-mu4e.el
+++ .emacs.d/lisp/init-mu4e.el
@@ -1,27 +1,31 @@
 ;;; init-mu4e --- mu4e configuration
 ;;; Commentary:
 ;;; Code:
+(add-to-list 'load-path "~/.emacs.d/site-lisp/mu4e")
 (require 'mu4e)
 (require-package 'helm-mu)
 (require-package 'mu4e-maildirs-extension)
 
 (setq mu4e-maildir                  "~/Maildir"
       mu4e-attachment-dir           "~/Maildir/Downloads"
-      mu4e-sent-folder              "/Work/INBOX.Sent"
-      mu4e-drafts-folder            "/Work/INBOX.Drafts"
       mu4e-get-mail-command         "offlineimap"
-      mu4e-user-mail-address-regexp "mywork-addres\\|mygmail-address"
+      mu4e-user-mail-address-regexp "my-email-address"
       mu4e-html2text-command        "html2text -utf8 -width 72"
       mu4e-update-interval 300
       mu4e-use-fancy-chars nil
       mu4e-view-show-images t
       mu4e-view-image-max-width 800
-      ;; mu4e-html2text-command nil
       mu4e-trash-folder (lambda (msg)
                           (cond
-                           ((string-match "/Work/"  (or (mu4e-message-field msg :maildir) "")) "/Work/INBOX.Trash")
-                           ((string-match "/Gmail/" (or (mu4e-message-field msg :maildir) "")) "/Gmail/[Gmail].Trash")
-                           )))
+                           ((string-match "/Gmail/" (or (mu4e-message-field msg :maildir) "")) "/Gmail/[Gmail].Trash")))
+      mu4e-maildir-shortcuts '(("/Gmail/Gmail"               . ?g)
+                               ("/Gmail/INBOX"               . ?i)
+                               ("/Gmail/[Gmail].Sent Mail"   . ?s)
+                               ("/Gmail/[Gmail].All Mail"    . ?a)
+                               ("/Work/Gmail"              . ?G)
+                               ("/Work/INBOX"              . ?I)
+                               ("/Work/[Work].Sent Mail" . ?S)
+                               ("/Work/[Work].All Mail"  . ?A)))
 (setq mail-user-agent 'mu4e-user-agent)
 (setq message-kill-buffer-on-exit t
       message-send-mail-function 'smtpmail-send-it)
@@ -30,64 +34,59 @@
 
 ;;; SMTPMail
 (require 'smtpmail)
+(defun my-mu4e-set-account ()
+  "Set the account for composing a message."
+  (let* ((account
+          (if mu4e-compose-parent-message
+              (let ((maildir (mu4e-message-field mu4e-compose-parent-message :maildir)))
+                (string-match "/\\(.*?\\)/" maildir)
+                (match-string 1 maildir))
+            (completing-read (format "Compose with account: (%s) "
+                                     (mapconcat #'(lambda (var) (car var))
+                                                my-mu4e-account-alist "/"))
+                             (mapcar #'(lambda (var) (car var)) my-mu4e-account-alist)
+                             nil t nil nil (caar my-mu4e-account-alist))))
+         (account-vars (cdr (assoc account my-mu4e-account-alist))))
+    (if account-vars
+        (mapc #'(lambda (var)
+                  (set (car var) (cadr var)))
+              account-vars)
+      (error "No email account found"))))
+;; ask for account when composing mail
+(add-hook 'mu4e-compose-pre-hook 'my-mu4e-set-account)
 (setq my-mu4e-account-alist
       '(("Gmail"
          (mu4e-sent-messages-behavior delete)
-         (mu4e-sent-folder   "/Gmail/[Gmail].Sent Mail")
-         (mu4e-drafts-folder "/Gmail/[Gmail].Drafts")
-         (user-mail-address  "mygmail-address")
+         (mu4e-sent-folder   "Gmail/[Gmail].Sent Mail")
+         (mu4e-drafts-folder "Gmail/[Gmail].Drafts")
+         (user-mail-address  "my-email-address")
+         (user-full-name  "Foo Manchu")
          (smtpmail-starttls-credentials  '(("smtp.gmail.com" 587 nil nil)))
-         (smtpmail-auth-credentials      '(("smtp.gmail.com" 587 "mygmail-address" nil)))
+         (smtpmail-auth-credentials      '(("smtp.gmail.com" 587 "my-email-address" nil)))
          (smtpmail-default-smtp-server "smtp.gmail.com")
-         (smtpmail-smtp-user           "mygmail-address")
+         (smtpmail-smtp-user           "my-email-address")
+         (smtpmail-smtp-server         "smtp.gmail.com")
+         (smtpmail-smtp-service 587)
+         (starttls-use-gnutls t)
+         (message-signature nil)
+         ;; add other variables here
+         )
+        ("Work"
+         (mu4e-sent-messages-behavior delete)
+         (mu4e-sent-folder   "Work/[Work].Sent Mail")
+         (mu4e-drafts-folder "Work/[Work].Drafts")
+         (user-mail-address  "work-email-address")
+         (user-full-name  "Foo Manchu")
+         (smtpmail-starttls-credentials  '(("smtp.gmail.com" 587 nil nil)))
+         (smtpmail-auth-credentials      '(("smtp.gmail.com" 587 "work-email-address" nil)))
+         (smtpmail-default-smtp-server "smtp.gmail.com")
+         (smtpmail-smtp-user           "work-email-address")
          (smtpmail-smtp-server         "smtp.gmail.com")
          (smtpmail-smtp-service 587)
          (starttls-use-gnutls t)
          (message-signature nil)
          ;; add other variables here
          )))
-
-
-;; ;;; Shortcut Keys
-;; (add-to-list 'jk-mu4e-account-alist
-;;              '("Work"
-;;                (mu4e-sent-folder   "/Work/INBOX.Sent")
-;;                (mu4e-drafts-folder "/Work/INBOX.Drafts")
-;;                (smtpmail-smtp-user "work-username")
-;;                (user-mail-address  "work-address")
-;;                (mu4e-sent-messages-behavior sent)
-;;                (starttls-use-gnutls nil)
-;;                (smtpmail-default-smtp-server "work-smtpserver")
-;;                (smtpmail-smtp-server         "worksmtpserver")
-;;                (smtpmail-smtp-service secret)
-;;                (message-signature (shell-command-to-string "cat ~/Mail/signature.txt"))
-;;                ;; add other variables here
-;;                ))
-;; (defun jk-mu4e-set-account ()
-;;   "Set the account for composing a message."
-;;   (let* ((account
-;;           (if mu4e-compose-parent-message
-;;     (let ((maildir (mu4e-msg-field mu4e-compose-parent-message :maildir)))
-;;                 (string-match "/\\(.*?\\)/" maildir)
-;;                 (match-string 1 maildir))
-;;             (completing-read (format "Compose with account: (%s) "
-;;                                      (mapconcat #'(lambda (var) (car var)) jk-mu4e-account-alist "/"))
-;;                              (mapcar #'(lambda (var) (car var)) jk-mu4e-account-alist)
-;;                              nil t nil nil (caar jk-mu4e-account-alist))))
-;;          (account-vars (cdr (assoc account jk-mu4e-account-alist))))
-;;     (if account-vars
-;;     (mapc #'(lambda (var)
-;;   (set (car var) (cadr var)))
-;;               account-vars))))
-;; (add-hook 'mu4e-compose-pre-hook 'jk-mu4e-set-account)
-;; (setq mu4e-maildir-shortcuts
-;;       '(("/Work/INBOX"              . ?i)
-;;         ("/Work/INBOX.Sent"         . ?s)
-;;         ("/Gmail/Gmail"             . ?g)
-;;         ("/Gmail/INBOX"             . ?n)
-;;         ("/Gmail/[Gmail].Sent Mail" . ?e)
-;;         ("/Gmail/[Gmail].All Mail"  . ?a)
-;;         ))
 
 
 ;;; Org-MU4E
@@ -109,7 +108,6 @@
   (let* ((netrc (netrc-parse (expand-file-name "~/.authinfo.gpg")))
          (hostentry (netrc-machine netrc host port port)))
     (when hostentry (netrc-get hostentry "password"))))
-
 
 
 (provide 'init-mu4e)
