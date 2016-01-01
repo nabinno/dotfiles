3f7291ad3992dae572ad4d8756196ffcaee9a59f
Nab Inno <nab@blahfe.com>
2016-01-01 17:19:22 +0900

    [fix]inf-php and nvm commands.

--- .emacs.d/lisp/init-php.el
+++ .emacs.d/lisp/init-php.el
@@ -4,10 +4,6 @@
 
 ;;; Inferior php
 (require-package 'inf-php)
-(require-package 'ac-inf-php)
-(after-load 'auto-complete
-  (add-to-list 'ac-modes 'inf-php-mode))
-(add-hook 'inf-php-mode-hook 'ac-inf-php-enable)
 (after-load 'inf-php
   (define-key inf-php-mode-map (kbd "TAB") 'auto-complete))
 
