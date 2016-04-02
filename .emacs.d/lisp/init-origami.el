0f91c0e459cd2d2884bbf12fb519b94aac2914c3
nabinno <nab@blahfe.com>
2016-04-02 11:59:52 +0900

    Fix origami-modes to mode-hooks.

--- .emacs.d/lisp/init-origami.el
+++ .emacs.d/lisp/init-origami.el
@@ -3,38 +3,15 @@
 ;;; Code:
 (require-package 'origami)
 
-(add-hook 'after-init-hook
-          (lambda ()
-            (progn
-              (origami-mode)
-              (dolist (hook
-                       '(caml-mode-hook
-                         clojure-mode-hook
-                         crontab-mode-hook
-                         css-mode-hook
-                         emacs-lisp-mode-hook
-                         haskell-interactive-mode-hook
-                         haskell-mode-hook
-                         haskell-mode-hook
-                         inferior-haskell-mode-hook
-                         javascript-mode-hook
-                         js-mode-hook
-                         js2-mode-hook
-                         lisp-mode-hook
-                         nxml-mode-hook
-                         perl-mode-hook
-                         php-mode-hook
-                         python-mode-hook
-                         ruby-mode-hook
-                         scheme-mode-hook
-                         tcl-mode-hook
-                         yaml-mode
-                         ))
-                (add-hook hook
-                          (lambda ()
-                            (progn (origami-wrap-mode)
-                                   (origami-cycle 1)))))
-              )))
+(dolist (mode '(caml clojure crontab css emacs-lisp haskell-interactive haskell
+                     haskell inferior-haskell javascript js js2 lisp nxml perl
+                     php python ruby scheme tcl yaml))
+  (add-hook (intern (format "%s-mode-hook" mode))
+            '(lambda ()
+               (origami-mode)
+               (origami-wrap-mode)
+               (origami-cycle 1))))
+
 ;; (add-hook 'view-mode-hook 'view-mode-hook--origami)
 
 
