68b42a92fec70b65114ed34d47a0ed00f50c8326
Nab Inno <nab@blahfe.com>
2016-04-04 01:03:41 +0900

    Add init-esa.el.

---
+++ .emacs.d/lisp/init-esa.el
@@ -0,0 +1,8 @@
+;;; init-esa --- esa configuration
+;;; Commentary:
+;;; Code:
+(unless (require 'esa nil 'noerror)
+    (el-get-bundle nabinno/esa.el))
+
+(provide 'init-esa)
+;;; init-esa.el ends here
