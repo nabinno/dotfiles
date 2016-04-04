936a24012f3c362532d3e6fccdb26b0b8f537463
nabinno <nab@blahfe.com>
2016-04-04 22:08:53 +0900

    Add global-set-key for esa.el.

--- .emacs.d/lisp/init-esa.el
+++ .emacs.d/lisp/init-esa.el
@@ -4,5 +4,9 @@
 (unless (require 'esa nil 'noerror)
     (el-get-bundle nabinno/esa.el))
 
+(global-set-key (kbd "C-c ; e") 'esa-list)
+(global-set-key (kbd "C-c ; E") 'esa-buffer-wip)
+
+
 (provide 'init-esa)
 ;;; init-esa.el ends here
