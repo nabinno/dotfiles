c9122fe90c6369e8598f0e77de17ab961534a3a1
nabinno <nab@blahfe.com>
2017-09-23 20:21:00 +0900

    Add packages: jedi, py-autopep8 and pyflakes.

--- .emacs.d/lisp/init-python-mode.el
+++ .emacs.d/lisp/init-python-mode.el
@@ -1,7 +1,34 @@
+;;; init-python-mode --- python-mode configuration
+;;; Commentary:
+;;; Code:
 (setq auto-mode-alist
       (append '(("SConstruct\\'" . python-mode)
 		("SConscript\\'" . python-mode))
               auto-mode-alist))
 
+
+;;; Jedi
+(require-package 'jedi)
+(add-hook 'python-mode-hook 'jedi:setup)
+(setq jedi:complete-on-dot t)
+(setq ac-sources
+      (delete 'ac-source-words-in-same-mode-buffers ac-sources))
+(add-to-list 'ac-sources 'ac-source-filename)
+(add-to-list 'ac-sources 'ac-source-jedi-direct)
+(define-key jedi-mode-map (kbd "<C-tab>") nil)
+
+
+;;; Py-Autopep8
+(require-package 'py-autopep8)
+(setq py-autopep8-options '("--max-line-length=200"))
+(setq flycheck-flake8-maximum-line-length 200)
+(py-autopep8-enable-on-save)
+
+
+;;; PyFlakes
+(require-package 'flymake-python-pyflakes)
+(flymake-python-pyflakes-load)
+
 
 (provide 'init-python-mode)
+;;; init-python-mode.el ends here
