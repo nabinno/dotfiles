917f320c50b367e522dee51ce59e61d51edc5818
Nab Inno <nab@blahfe.com>
2017-10-22 17:34:54 +0900

    Update init-crontab configurations.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -80,7 +80,7 @@
 (require 'init-git)
 (require 'init-github)
 
-(require 'init-crontab)
+;; (require 'init-crontab) ;;;
 ;; (require 'init-textile)
 (require 'init-markdown)
 (require 'init-csv)
@@ -142,7 +142,7 @@
 (require-package 'regex-tool)
 
 (require 'init-irc)
-;; (require 'init-esa)
+(require 'init-esa)
 (require 'init-proced)
 
 ;;----------------------------------------------------------------------------
--- .emacs.d/lisp/init-crontab.el
+++ .emacs.d/lisp/init-crontab.el
@@ -1,4 +1,11 @@
+;;; init-crontab --- configuration of crontab-mode
+;;; Commentary:
+;;; Code:
+
 (require-package 'crontab-mode)
 (add-auto-mode 'crontab-mode "\\.?cron\\(tab\\)?\\'")
 
+
+
 (provide 'init-crontab)
+;;; init-crontab ends here
