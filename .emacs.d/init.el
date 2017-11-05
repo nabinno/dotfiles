ba862172efcd5b9975a3f333a31f4e607984c3b4
Nab Inno <nab@blahfe.com>
2017-11-05 12:53:42 +0900

    Add init-web.el.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -71,7 +71,8 @@
 (require 'init-elscreen)
 ;; (require 'init-sessions)
 ;; (require 'init-fonts)
-(require 'init-mmm)
+;; (require 'init-mmm)
+(require 'init-web)
 
 (require 'init-editing-utils)
 
@@ -80,7 +81,7 @@
 (require 'init-git)
 (require 'init-github)
 
-;; (require 'init-crontab) ;;;
+(require 'init-crontab)
 ;; (require 'init-textile)
 (require 'init-markdown)
 (require 'init-csv)
@@ -142,7 +143,7 @@
 (require-package 'regex-tool)
 
 (require 'init-irc)
-(require 'init-esa)
+;; (require 'init-esa)
 (require 'init-proced)
 
 ;;----------------------------------------------------------------------------
