8f3d4028178c7b88ffd9b1adb0b8f85d5c931ad5
nabinno <nab@blahfe.com>
2017-11-05 22:30:41 +0900

    Add init-web.

--- .emacs.d/lisp/init-css.el
+++ .emacs.d/lisp/init-css.el
@@ -8,34 +8,32 @@
     (add-hook hook 'rainbow-mode)))
 
 
-;;; Embedding in html
-(require-package 'mmm-mode)
-(after-load 'mmm-vars
-  (mmm-add-group
-   'html-css
-   '((css-cdata
-      :submode css-mode
-      :face mmm-code-submode-face
-      :front "<style[^>]*>[ \t\n]*\\(//\\)?<!\\[CDATA\\[[ \t]*\n?"
-      :back "[ \t]*\\(//\\)?]]>[ \t\n]*</style>"
-      :insert ((?j js-tag nil @ "<style type=\"text/css\">"
-                   @ "\n" _ "\n" @ "</script>" @)))
-     (css
-      :submode css-mode
-      :face mmm-code-submode-face
-      :front "<style[^>]*>[ \t]*\n?"
-      :back "[ \t]*</style>"
-      :insert ((?j js-tag nil @ "<style type=\"text/css\">"
-                   @ "\n" _ "\n" @ "</style>" @)))
-     (css-inline
-      :submode css-mode
-      :face mmm-code-submode-face
-      :front "style=\""
-      :back "\"")))
-  (dolist (mode (list 'html-mode 'nxml-mode))
-    (mmm-add-mode-ext-class mode "\\.r?html\\(\\.erb\\)?\\'" 'html-css)))
-
-
+;; ;;; Embedding in html
+;; (require-package 'mmm-mode)
+;; (after-load 'mmm-vars
+;;   (mmm-add-group
+;;    'html-css
+;;    '((css-cdata
+;;       :submode css-mode
+;;       :face mmm-code-submode-face
+;;       :front "<style[^>]*>[ \t\n]*\\(//\\)?<!\\[CDATA\\[[ \t]*\n?"
+;;       :back "[ \t]*\\(//\\)?]]>[ \t\n]*</style>"
+;;       :insert ((?j js-tag nil @ "<style type=\"text/css\">"
+;;                    @ "\n" _ "\n" @ "</script>" @)))
+;;      (css
+;;       :submode css-mode
+;;       :face mmm-code-submode-face
+;;       :front "<style[^>]*>[ \t]*\n?"
+;;       :back "[ \t]*</style>"
+;;       :insert ((?j js-tag nil @ "<style type=\"text/css\">"
+;;                    @ "\n" _ "\n" @ "</style>" @)))
+;;      (css-inline
+;;       :submode css-mode
+;;       :face mmm-code-submode-face
+;;       :front "style=\""
+;;       :back "\"")))
+;;   (dolist (mode (list 'html-mode 'nxml-mode))
+;;     (mmm-add-mode-ext-class mode "\\.r?html\\(\\.erb\\)?\\'" 'html-css)))
 
 
 ;;; SASS and SCSS
--- .emacs.d/lisp/init-ruby-mode.el
+++ .emacs.d/lisp/init-ruby-mode.el
@@ -80,34 +80,34 @@
 (require-package 'slim-mode)
 
 
-;;; ERB
-(require-package 'mmm-mode)
-(defun sanityinc/ensure-mmm-erb-loaded ()
-  (require 'mmm-erb))
-
-(require 'derived)
-
-(defun sanityinc/set-up-mode-for-erb (mode)
-  (add-hook (derived-mode-hook-name mode) 'sanityinc/ensure-mmm-erb-loaded)
-  (mmm-add-mode-ext-class mode "\\.erb\\'" 'erb))
-
-(let ((html-erb-modes '(html-mode html-erb-mode nxml-mode)))
-  (dolist (mode html-erb-modes)
-    (sanityinc/set-up-mode-for-erb mode)
-    (mmm-add-mode-ext-class mode "\\.r?html\\(\\.erb\\)?\\'" 'html-js)
-    (mmm-add-mode-ext-class mode "\\.r?html\\(\\.erb\\)?\\'" 'html-css)))
-
-(mapc 'sanityinc/set-up-mode-for-erb
-      '(coffee-mode js-mode js2-mode js3-mode markdown-mode textile-mode))
-
-(mmm-add-mode-ext-class 'html-erb-mode "\\.jst\\.ejs\\'" 'ejs)
-
-(add-auto-mode 'html-erb-mode "\\.rhtml\\'" "\\.html\\.erb\\'")
-(add-to-list 'auto-mode-alist '("\\.jst\\.ejs\\'"  . html-erb-mode))
-(mmm-add-mode-ext-class 'yaml-mode "\\.yaml\\'" 'erb)
-
-(dolist (mode (list 'js-mode 'js2-mode 'js3-mode))
-  (mmm-add-mode-ext-class mode "\\.js\\.erb\\'" 'erb))
+;; ;;; ERB
+;; (require-package 'mmm-mode)
+;; (defun sanityinc/ensure-mmm-erb-loaded ()
+;;   (require 'mmm-erb))
+;;
+;; (require 'derived)
+;;
+;; (defun sanityinc/set-up-mode-for-erb (mode)
+;;   (add-hook (derived-mode-hook-name mode) 'sanityinc/ensure-mmm-erb-loaded)
+;;   (mmm-add-mode-ext-class mode "\\.erb\\'" 'erb))
+;;
+;; (let ((html-erb-modes '(html-mode html-erb-mode nxml-mode)))
+;;   (dolist (mode html-erb-modes)
+;;     (sanityinc/set-up-mode-for-erb mode)
+;;     (mmm-add-mode-ext-class mode "\\.r?html\\(\\.erb\\)?\\'" 'html-js)
+;;     (mmm-add-mode-ext-class mode "\\.r?html\\(\\.erb\\)?\\'" 'html-css)))
+;;
+;; (mapc 'sanityinc/set-up-mode-for-erb
+;;       '(coffee-mode js-mode js2-mode js3-mode markdown-mode textile-mode))
+;;
+;; (mmm-add-mode-ext-class 'html-erb-mode "\\.jst\\.ejs\\'" 'ejs)
+;;
+;; (add-auto-mode 'html-erb-mode "\\.rhtml\\'" "\\.html\\.erb\\'")
+;; (add-to-list 'auto-mode-alist '("\\.jst\\.ejs\\'"  . html-erb-mode))
+;; (mmm-add-mode-ext-class 'yaml-mode "\\.yaml\\'" 'erb)
+;;
+;; (dolist (mode (list 'js-mode 'js2-mode 'js3-mode))
+;;   (mmm-add-mode-ext-class mode "\\.js\\.erb\\'" 'erb))
 
 
 ;;; My convention for heredocs containing SQL
