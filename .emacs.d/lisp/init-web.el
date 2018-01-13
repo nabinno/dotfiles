ca8f82f3cb2284c7f75c8c9b390e9b182beddf1f
nabinno <nab@blahfe.com>
2018-01-13 22:58:48 +0900

    Add eex file to list.

--- .emacs.d/lisp/init-web.el
+++ .emacs.d/lisp/init-web.el
@@ -9,6 +9,7 @@
 (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.eex\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
