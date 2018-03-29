7a2c6f9676e256cbae2b2b4f17ab408b75756213
nabinno <nab@blahfe.com>
2018-03-29 17:30:58 +0900

    Add apib-mode.

---
+++ .emacs.d/lisp/init-apiblueprint.el
@@ -0,0 +1,11 @@
+;;; init-apiblueprint --- API Blueprint configuration file
+;;; Commentary:
+;;; Code:
+
+(unless (require 'apib-mode nil 'noerror)
+  (el-get-bundle w-vi/apib-mode))
+
+
+
+(provide 'init-apiblueprint)
+;;; init-apiblueprint.el ends here
