7012a2f174417be7bb7dac535dab6a798ad990f9
nabinno <nab@blahfe.com>
2018-04-01 04:05:12 +0900

    Add configurations for ripgrep.

--- .emacs.d/lisp/init-helm.el
+++ .emacs.d/lisp/init-helm.el
@@ -3,6 +3,23 @@
 ;;; Code:
 (require-package 'helm)
 
+
+;; RipGrep
+(require-package 'helm-ag)
+
+(setq helm-ag-base-command "rg --vimgrep --no-heading")
+(setq helm-ag-insert-at-point 'symbol) ; set up current symbol to default query
+(defun helm-ag-dot-emacs ()
+  "Search .emacs.d directory."
+  (interactive)
+  (helm-ag "~/.emacs.d/"))
+(defun helm-projectile-ag ()
+  "Connect to projectile."
+  (interactive)
+  (helm-ag (projectile-project-root)))
+(global-set-key (kbd "C-c ; G") 'helm-projectile-ag)
+(global-set-key (kbd "C-c ; E") 'helm-ag-dot-emacs)
+
 
 
 (provide 'init-helm)
