51829db80131215b2995a0720689f54a3b78814c
nabinno <nab@blahfe.com>
2018-06-30 12:28:02 +0900

    Apply autoload.

--- .zsh.d/function/init-shell.zsh
+++
@@ -1,9 +0,0 @@
-function get-shellcheck() {
-  case $OSTYPE in
-    linux*)
-      case $DIST in
-        Ubuntu) sudo apt-get install shellcheck ;;
-      esac
-      ;;
-  esac
-}
--- .zsh.d/function/init-slack.zsh
+++
@@ -1,8 +0,0 @@
-get-slackchat() {
-  case "${OSTYPE}" in
-    darwin* | linux*)
-      wget https://github.com/vektorlab/slackcat/releases/download/v0.7/slackcat-0.7-linux-amd64 -O ~/.local/bin/slackchat
-      chmod +x ~/.local/bin/slackchat
-      ;;
-  esac
-}
