bae8bdbcd363541eb189520017800fe9ee00512b
nabinno <nab@blahfe.com>
2018-08-26 12:22:25 +0900

    Apply prettier to web-mode.

--- .emacs.d/lisp/init-web.el
+++ .emacs.d/lisp/init-web.el
@@ -35,6 +35,17 @@
                                  )))
 (add-hook 'web-mode-hook  'web-mode-hook)
 
+
+;; prettier
+(defun enable-minor-mode (my-pair)
+  "Enable minor mode if filename match the regexp.
+MY-PAIR is a cons cell (regexp . minor-mode)."
+  (if (buffer-file-name)
+      (if (string-match (car my-pair) buffer-file-name)
+          (funcall (cdr my-pair)))))
+(add-hook 'web-mode-hook #'(lambda () (enable-minor-mode '("\\.jsx?\\'" . prettier-js-mode))))
+(add-hook 'web-mode-hook #'(lambda () (enable-minor-mode '("\\.eex\\'" . prettier-js-mode))))
+
 
 (provide 'init-web)
 ;;; init-web.el ends here
