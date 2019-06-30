a310ab194393cde9bddf6a6144cc6be98f3f87e7
nabinno <nab@blahfe.com>
2019-06-30 18:44:31 +0900

    Add .emacs file on NT.

---
+++ .nt/appdata/.emacs
@@ -0,0 +1,48 @@
+;;; Package configuration
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+;; straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+
+
+;;; Local configuration
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(inhibit-startup-screen t)
+ '(menu-bar-mode nil)
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil)
+ '(truncate-lines t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "Black" :foreground "White" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 102 :width normal :foundry "outline" :family "MS Gothic"))))
+ '(cursor ((t (:background "green")))))
+
+
+
+;;; Misc initialization
+(straight-use-package
+ '(el-patch :type git :host github :repo "jschaf/powershell.el"))
---
+++ .nt/mydocuments/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,182 @@
+﻿# == INDEX
+# Base::EnvironmentVariable
+# PackageManagement::WindowsPackageManagement
+# PackageManagement::WindowsPackageManagement::Chocolatey
+# PackageManagement::Docker
+# ProgrammingLanguage::DotNet
+# ProgrammingLanguage::DotNet::UniversalWindowsPlatform
+# IntegratedDevelopmentEnvironment::Shell
+# IntegratedDevelopmentEnvironment::Editor::Emacs
+# IntegratedDevelopmentEnvironment::Editor::Vi
+# IntegratedDevelopmentEnvironment::ResourceManagement::Git
+# IntegratedDevelopmentEnvironment::Network
+# IntegratedDevelopmentEnvironment::OsLevelVirtualization::HyperV
+# Other
+
+
+# Base::EnvironmentVariable
+
+
+# PackageManagement::WindowsPackageManagement
+Set-Alias gpkg Get-Package
+Set-Alias fpkg Find-Package
+
+
+# PackageManagement::WindowsPackageManagement::Chocolatey
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+
+# PackageManagement::Docker
+function dp { docker ps -a }
+function dl { docker images -a }
+
+
+# ProgrammingLanguage::DotNet
+function Set-AspNetProject {
+    npm install yo -g
+    npm install generator-aspnet -g
+    yo aspnet
+    # dnx . run
+}
+function Install-OmniSharp {
+    git clone https://github.com/OmniSharp/omnisharp-server.git
+    cd omnisharp-server
+    git submodule update --init --recursive
+    msbuild
+    cd ..
+    mv omnisharp-server ~/OneDrive/bin/
+}
+
+
+# ProgrammingLanguage::DotNet::UniversalWindowsPlatform
+function Set-AppXPackages {
+  Get-AppxPackage | foreach { Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml" }
+}
+
+
+# IntegratedDevelopmentEnvironment::Shell
+Set-Alias bash 'C:\Windows\System32\bash.exe'
+
+
+# IntegratedDevelopmentEnvironment::Editor::Emacs
+function f { emacs -nw args[1] }
+Set-PSReadlineOption -EditMode Emacs
+
+
+# IntegratedDevelopmentEnvironment::Editor::Vi
+Set-Alias e 'vim'
+
+
+# IntegratedDevelopmentEnvironment::ResourceManagement::Git
+function ga { git add args[1] }
+function gaa { git add . }
+function gct { git commit -m "args[1]" }
+function gd { git diff }
+function gdf { git diff }
+function glg { git log }
+function gs { git status -sb }
+Set-Alias g git
+
+
+# IntegratedDevelopmentEnvironment::Network
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+function Edit-Hosts { vim 'C:\Windows\System32\drivers\etc\hosts' }
+function Start-PortForwarding {
+    Start-Job -Name 'PortForwarding' -ScriptBlock {
+        ssh -N -L 3000:localhost:3000 -L 443:localhost:443 vagrant@fe80::215:5dff:fe00:4100
+    }
+}
+function Sync-HostsToWslIp {
+    $hosts = "C:\Windows\System32\drivers\etc\hosts";
+    $pattern = "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}";
+    $wslip = bash.exe -c "ifconfig eth0 | grep 'inet '";
+    if ($wslip -match $pattern) {
+	$wslip = $matches[0];
+    } else {
+	echo "The Script Exited, the ip address of WSL 2 cannot be found";
+	exit;
+    }
+    cat $hosts | %{ $_ -match $pattern }
+    $rc = cat $hosts | %{ $_ -replace $matches[0], $wslip }
+    $rc | Out-File $hosts;
+}
+Set-Alias eh Edit-Hosts
+Set-Alias gna Get-NetAdapter
+Set-Alias spf Start-PortForwarding
+Set-Alias rna Restart-NetAdapter
+Set-Alias shw Sync-HostsToWslIp
+
+
+# IntegratedDevelopmentEnvironment::OsLevelVirtualization::HyperV
+function Connect-VM {
+    [CmdletBinding(DefaultParameterSetName='name')]
+    param(
+        [Parameter(ParameterSetName='name')] [Alias('cn')] [System.String[]]$ComputerName=$env:COMPUTERNAME,
+        [Parameter(Position=0, Mandatory, ValueFromPipelineByPropertyName, ValueFromPipeline, ParameterSetName='name')] [Alias('VMName')]      [System.String]$Name,
+        [Parameter(Position=0, Mandatory, ValueFromPipelineByPropertyName, ValueFromPipeline, ParameterSetName='id')]   [Alias('VMId','Guid')] [System.Guid]$Id,
+        # [Parameter(Position=0, Mandatory, ValueFromPipeline,                                  ParameterSetName='inputObject')] [Microsoft.HyperV.PowerShell.VirtualMachine]$InputObject,
+        [switch]$StartVM
+    )
+    begin {
+        Write-Verbose "Initializing InstanceCount, InstanceCount = 0"
+        $InstanceCount=0
+    } process {
+        try {
+            foreach($computer in $ComputerName) {
+                Write-Verbose "ParameterSetName is '$($PSCmdlet.ParameterSetName)'"
+                if ($PSCmdlet.ParameterSetName -eq 'name') {
+                              # Get the VM by Id if Name can convert to a guid
+                    if ($Name -as [guid]) {
+                        Write-Verbose "Incoming value can cast to guid"
+                        $vm = Get-VM -Id $Name -ErrorAction SilentlyContinue
+                    } else {
+                        $vm = Get-VM -Name $Name -ErrorAction SilentlyContinue
+                    }
+                } elseif ($PSCmdlet.ParameterSetName -eq 'id') {
+                    $vm = Get-VM -Id $Id -ErrorAction SilentlyContinue
+                } else {
+                    $vm = $InputObject
+                } if ($vm) {
+                    Write-Verbose "Executing 'vmconnect.exe $computer $($vm.Name) -G $($vm.Id) -C $InstanceCount'"
+                    vmconnect.exe $computer $vm.Name -G $vm.Id -C $InstanceCount
+                } else {
+                    Write-Verbose "Cannot find vm: '$Name'"
+                } if ($StartVM -and $vm) {
+                    if ($vm.State -eq 'off') {
+                        Write-Verbose "StartVM was specified and VM state is 'off'. Starting VM '$($vm.Name)'"
+                        Start-VM -VM $vm
+                    } else {
+                        Write-Verbose "Starting VM '$($vm.Name)'. Skipping, VM is not not in 'off' state."
+                    }
+                }
+                $InstanceCount+=1
+                Write-Verbose "InstanceCount = $InstanceCount"
+            }
+        } catch {
+            Write-Error $_
+        }
+    }
+}
+Set-Alias gvm Get-VM
+Set-Alias gvmn Get-VMNetworkAdapter
+
+
+# Other
+function d { Remove-Item args[1] -Recurse -Force }
+function re { vim $PROFILE }
+function rr { . $PROFILE }
+function ll { Get-ChildItem -Exclude .* -Name }
+function lf { Get-ChildItem -Exclude .* }
+function la { Get-ChildItem -Exclude .*; Get-ChildItem -Hidden }
+function ~ { Set-Location ~ }
+function .. { Set-Location .. }
+Set-Alias ggl google
+Set-Alias i Get-Help
+Set-Alias p Write-Output
+Set-Alias ip Get-Process
+Set-Alias j Set-Location
+Set-Alias v Get-Content
+Set-Alias which Get-Command
