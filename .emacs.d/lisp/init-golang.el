9ef4f56fbb07fbf752d3a289d9c080013c3e2bcc
nabinno <nab@blahfe.com>
2019-08-02 07:41:47 +0900

    Add golang configurations.

---
+++ .emacs.d/lisp/init-golang.el
@@ -0,0 +1,11 @@
+;;; init-golang -- basic golang configuration
+;;; Commentary:
+;;; Code:
+; @todo 2019-08-02
+(require-package 'go-mode)
+(require-package 'eglot)
+
+
+
+(provide 'init-golang)
+;;; init-golang.el ends here
