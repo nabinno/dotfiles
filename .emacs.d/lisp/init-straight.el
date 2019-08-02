e4007b0e6e4b91c072a34f2b37c906b1caed0a3e
nabinno <nab@blahfe.com>
2019-08-03 08:50:07 +0900

    Add configuration of straight.el.

--- .emacs.d/lisp/init-straight.el
+++ .emacs.d/lisp/init-straight.el
@@ -14,5 +14,27 @@
 	  (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
+
+;; use-package
+(straight-use-package 'use-package)
+(use-package use-package-ensure-system-package :straight t)
+
+
+;; base
+(defun jjin/use-package-if-prehook (name _keyword pred rest state)
+  (unless pred (error "predicated failed; skipping package")))
+(advice-add 'use-package-handler/:if :before 'jjin/use-package-if-prehook)
+
+(use-package diminish :straight t)
+(use-package names :straight t)
+(use-package system-packages
+  :straight t
+  :init
+  (setq system-packages-use-sudo nil)
+  (when (eq system-type 'darwin)
+    (setq system-packages-package-manager 'brew)))
+
+
+
 (provide 'init-straight)
 ;;; init-straight.el ends here
