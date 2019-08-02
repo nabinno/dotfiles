4e90a67d7884b949500a72f9749ba13b6b4e3089
nabinno <nab@blahfe.com>
2019-08-03 08:50:02 +0900

    Add golang configurations.

--- .emacs.d/lisp/init-golang.el
+++ .emacs.d/lisp/init-golang.el
@@ -2,8 +2,21 @@
 ;;; Commentary:
 ;;; Code:
 ; @todo 2019-08-02
-(require-package 'go-mode)
-(require-package 'eglot)
+(use-package go-mode
+  :mode "\\.go$"
+  :straight t
+  :init (if (executable-find "goimports")
+            (setq gofmt-command "goimports")
+          (message "Goimports not found; using default `gofmt-command'"))
+  :config (add-hook 'before-save-hook 'gofmt-before-save))
+
+(use-package company-go
+  :after go
+  :disabled t)
+
+(use-package go-guru
+  :after go
+  :straight t)
 
 
 
