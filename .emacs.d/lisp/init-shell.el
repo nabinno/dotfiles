393c3a35e65ceedd771a93b035cdcb0c299bf717
nabinno <nab@blahfe.com>
2019-08-05 07:33:55 +0900

    Add shell-command-current-file.

--- .emacs.d/lisp/init-shell.el
+++ .emacs.d/lisp/init-shell.el
@@ -16,6 +16,49 @@
       (shell-command (concat "shfmt -i 2 -ci -w " (buffer-file-name)))))
 (add-hook 'after-save-hook 'shfmt-after-save)
 
+
+;; Other
+(defun shell-command-current-file ()
+  "Execute the current file.
+For example, if the current buffer is the file x.py, then it'll
+call 「python x.py」 in a shell.  The file can be Emacs Lisp,
+PHP, Perl, Python, Ruby, JavaScript, Bash, Ocaml, Visual Basic,
+TeX, Java, Clojure.  File suffix is used to determine what
+program to run.
+
+If the file is modified or not saved, save it automatically before run.
+
+URL `http://ergoemacs.org/emacs/elisp_run_current_file.html'
+version 2016-01-28"
+  (interactive)
+  (let (
+         (-suffix-map
+          `(("php" . "php")
+            ("pl" . "perl")
+            ("py" . "python")
+            ("rb" . "ruby")
+            ("go" . "go run")
+            ("js" . "node") ; node.js
+            ("sh" . "bash")
+            ("ml" . "ocaml")
+            ("tex" . "pdflatex")
+            ("latex" . "pdflatex")))
+         -fname
+         -fSuffix
+         -prog-name
+         -cmd-str)
+    (when (null (buffer-file-name)) (save-buffer))
+    (when (buffer-modified-p) (save-buffer))
+    (setq -fname (buffer-file-name))
+    (setq -fSuffix (file-name-extension -fname))
+    (setq -prog-name (cdr (assoc -fSuffix -suffix-map)))
+    (setq -cmd-str (concat -prog-name " \""   -fname "\""))
+    (if -prog-name
+        (progn
+          (message "Running…")
+          (shell-command -cmd-str "*xah-run-current-file output*" ))
+      (message "No recognized program file suffix for this file."))))
+
 
 
 (provide 'init-shell)
