b3a18fc6fe5b2efc337bbaeacf168806f4d40635
nabinno <nab@blahfe.com>
2019-08-23 18:08:21 +0900

    Exclude magefile.go from flycheckers (go-errcheck and go-build).

--- .emacs.d/lisp/init-flycheck.el
+++ .emacs.d/lisp/init-flycheck.el
@@ -1,13 +1,23 @@
+;;; init-flycheck.el --- flycheck configuration
+;;; Commentary:
+;;; Code:
 (when (maybe-require-package 'flycheck)
   (add-hook 'after-init-hook 'global-flycheck-mode)
+  (add-hook 'flycheck-mode-hook '(lambda ()
+                                   (when (and (buffer-file-name)
+                                              (string= (file-name-base (buffer-file-name)) "magefile"))
+                                     (flycheck-disable-checker 'go-errcheck)
+                                     (flycheck-disable-checker 'go-build)
+                                     )))
 
   ;; Override default flycheck triggers
   (setq flycheck-check-syntax-automatically '(save idle-change mode-enabled)
         flycheck-idle-change-delay 0.8)
 
-    (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list))
+  (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list))
 
 ;; (require-package 'flycheck-tip)
 
 
 (provide 'init-flycheck)
+;;; init-flycheck.el ends here
