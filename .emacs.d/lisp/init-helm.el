3e07f301456340c9ab09182146d36698e80383a2
nabinno <nab@blahfe.com>
2020-04-12 17:17:54 +0900

    Add helm-ag-dot-ghq-win32 functions.

--- .emacs.d/lisp/init-helm.el
+++ .emacs.d/lisp/init-helm.el
@@ -22,6 +22,10 @@
     "Search .ghq.d directory."
     (interactive)
     (progn (helm-ag "~/.ghq.d/") (delete-other-windows)))
+  (defun helm-ag-dot-ghq-win32 ()
+    "Search .ghq.d directory on win32."
+    (interactive)
+    (progn (helm-ag (concat (getenv "USER_PROFILE") "/.ghq.d/")) (delete-other-windows)))
   (defun helm-projectile-ag ()
     "Connect to projectile."
     (interactive)
@@ -29,6 +33,7 @@
   (global-set-key (kbd "C-c ; G") 'helm-projectile-ag)
   (global-set-key (kbd "C-c ; E") 'helm-ag-dot-emacs)
   (global-set-key (kbd "C-c ; H") 'helm-ag-dot-ghq)
+  (global-set-key (kbd "C-c ; W") 'helm-ag-dot-ghq-win32)
   (global-set-key (kbd "C-c ; Z") 'helm-ag-dot-zsh))
 
 
