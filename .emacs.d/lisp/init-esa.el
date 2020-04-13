b8d61f6c7c1a5fe30491de67039551f2af59ab77
nabinno <nab@blahfe.com>
2020-04-13 17:23:46 +0900

    Modify restclient.el configurations.

--- .emacs.d/lisp/init-esa.el
+++ .emacs.d/lisp/init-esa.el
@@ -7,6 +7,11 @@
 (global-set-key (kbd "C-c ; e b") 'esa-buffer-wip)
 (global-set-key (kbd "C-c ; e r") 'esa-region-wip)
 
+(add-hook 'esa-describe-write-mode-hook
+          (lambda ()
+            (define-key esa-describe-write-mode-map (kbd "C-c C-v") 'restclient-http-send-current-stay-in-window)))
+
+
 
 (provide 'init-esa)
 ;;; init-esa.el ends here
