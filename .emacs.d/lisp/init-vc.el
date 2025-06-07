f8cade774a5786442127a76321fa53cf29c21677
nabinno <nabinno@users.noreply.github.com>
2025-06-07 20:30:23 +0900

    Update diff-hl installation.

--- .emacs.d/lisp/init-vc.el
+++ .emacs.d/lisp/init-vc.el
@@ -1,9 +1,19 @@
 ;;; init-vc --- -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;; Code:
-(require-package 'diff-hl)
-(add-hook 'prog-mode-hook 'turn-on-diff-hl-mode)
-(add-hook 'vc-dir-mode-hook 'turn-on-diff-hl-mode)
+(leaf diff-hl
+  :ensure t
+  :hook ((prog-mode-hook . turn-on-diff-hl-mode)
+         (vc-dir-mode-hook . turn-on-diff-hl-mode))
+  :config
+  (global-diff-hl-mode +1)
+  (add-hook 'magit-pre-refresh-hook 'diff-hl-magit-pre-refresh)
+  (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
+  ;; For terminal use
+  (unless (window-system)
+    (diff-hl-margin-mode))
+  ;; Force refresh
+  (setq diff-hl-side 'left))
 
 
 (provide 'init-vc)
