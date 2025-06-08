54ca9fa694acae1f5c74989076e3ad85182d4139
nabinno <nabinno@users.noreply.github.com>
2025-06-08 14:55:23 +0900

    Change function `defadvice` to `advice-add`.

--- .emacs.d/lisp/init-editing-utils.el
+++ .emacs.d/lisp/init-editing-utils.el
@@ -239,25 +239,28 @@
     (and (boundp 'fci-mode) fci-mode))
 
   (defvar sanityinc/fci-mode-suppressed nil)
-  (defadvice popup-create (before suppress-fci-mode activate)
-    "Suspend fci-mode while popups are visible"
-    (let ((fci-enabled (sanityinc/fci-enabled-p)))
-      (when fci-enabled
-        (set (make-local-variable 'sanityinc/fci-mode-suppressed) fci-enabled)
-        (turn-off-fci-mode))))
-  (defadvice popup-delete (after restore-fci-mode activate)
-    "Restore fci-mode when all popups have closed"
-    (when (and sanityinc/fci-mode-suppressed
-               (null popup-instances))
-      (setq sanityinc/fci-mode-suppressed nil)
-      (turn-on-fci-mode)))
+  (advice-add 'popup-create :before
+              (lambda (&rest args)
+                "Suspend fci-mode while popups are visible"
+                (let ((fci-enabled (sanityinc/fci-enabled-p)))
+                  (when fci-enabled
+                    (set (make-local-variable 'sanityinc/fci-mode-suppressed) fci-enabled)
+                    (turn-off-fci-mode)))))
+  (advice-add 'popup-delete :after
+              (lambda (&rest args)
+                "Restore fci-mode when all popups have closed"
+                (when (and sanityinc/fci-mode-suppressed
+                           (null popup-instances))
+                  (setq sanityinc/fci-mode-suppressed nil)
+                  (turn-on-fci-mode))))
 
   ;; Regenerate fci-mode line images after switching themes
-  (defadvice enable-theme (after recompute-fci-face activate)
-    (dolist (buffer (buffer-list))
-      (with-current-buffer buffer
-        (when (sanityinc/fci-enabled-p)
-          (turn-on-fci-mode))))))
+  (advice-add 'enable-theme :after
+              (lambda (&rest args)
+                (dolist (buffer (buffer-list))
+                  (with-current-buffer buffer
+                    (when (sanityinc/fci-enabled-p)
+                      (turn-on-fci-mode)))))))
 
 
 ;;----------------------------------------------------------------------------
@@ -307,13 +310,15 @@
       `(progn
          (defvar ,flagvar nil)
          (make-variable-buffer-local ',flagvar)
-         (defadvice cua--activate-rectangle (after ,advice-name activate)
-           (setq ,flagvar (and (boundp ',mode-name) ,mode-name))
-           (when ,flagvar
-             (,mode-name 0)))
-         (defadvice cua--deactivate-rectangle (after ,advice-name activate)
-           (when ,flagvar
-             (,mode-name 1)))))))
+         (advice-add 'cua--activate-rectangle :after
+                     (lambda (&rest args)
+                       (setq ,flagvar (and (boundp ',mode-name) ,mode-name))
+                       (when ,flagvar
+                         (,mode-name 0))))
+         (advice-add 'cua--deactivate-rectangle :after
+                     (lambda (&rest args)
+                       (when ,flagvar
+                         (,mode-name 1))))))))
 
 (suspend-mode-during-cua-rect-selection 'whole-line-or-region-mode)
 
